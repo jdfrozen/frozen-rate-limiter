41b0835bc1ccc9707214b5984380a6b0d4a68a0c
jdfrozen <616769416@qq.com>
2019-01-03 20:30:39 +0800

    ''

--- J-frozen/sharding-jdbc/src/main/java/com/frozen/shardingjdbc/ShardingJdbcApplication.java
+++ J-frozen/sharding-jdbc/src/main/java/com/frozen/shardingjdbc/ShardingJdbcApplication.java
@@ -1,9 +1,11 @@
 package com.frozen.shardingjdbc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.frozen.shardingjdbc.dao")
 public class ShardingJdbcApplication {
 
     public static void main(String[] args) {
